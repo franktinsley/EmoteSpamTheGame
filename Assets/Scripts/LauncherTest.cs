718d53bcf1fa1b4816e330295acebe554adc4961
Frank Tinsley <franktinsley@me.com>
2016-10-11 10:06:58 -0700

    Add Unity project already in progress

---
+++ Assets/Scripts/LauncherTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class LauncherTest : MonoBehaviour
+{
+	public Launcher m_Launcher;
+	public KeyCode m_LaunchTestKey;
+
+	void Update()
+	{
+		if( Input.GetKeyDown( m_LaunchTestKey ) )
+		{
+			m_Launcher.Launch();
+		}
+	}
+}
---
+++ Assets/Scripts/Peg.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Peg : MonoBehaviour
+{
+	public int m_Health = 1;
+
+	void OnCollisionEnter( Collision colission )
+	{
+		m_Health--;
+		if( m_Health <= 0 )
+		{
+			Destroy( gameObject );
+		}
+	}
+}
