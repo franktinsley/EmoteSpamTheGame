e9ef0fb21e1ce12c7fae1716f0e23ae9fcdcd8e3
Frank Tinsley <franktinsley@me.com>
2016-10-31 17:41:11 -0700

    Rainbow jackpot

---
+++ Assets/Scripts/ColorExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public static class ColorExtensions
+{
+	public static Color WithHue( this Color color, float hue )
+	{
+		float _;
+		float saturation;
+		float value;
+		Color.RGBToHSV( color, out _, out saturation, out value );
+		return Color.HSVToRGB( hue, saturation, value );
+	}
+}
---
+++ Assets/Scripts/SpriteRendererHue.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[ RequireComponent( typeof( SpriteRenderer ) ) ]
+public class SpriteRendererHue : MonoBehaviour
+{
+	public float m_Hue;
+
+	SpriteRenderer m_SpriteRenderer;
+
+	void Start()
+	{
+		m_SpriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	void Update()
+	{
+		Color color = m_SpriteRenderer.color;
+		m_SpriteRenderer.color = color.WithHue( m_Hue );
+	}
+}
